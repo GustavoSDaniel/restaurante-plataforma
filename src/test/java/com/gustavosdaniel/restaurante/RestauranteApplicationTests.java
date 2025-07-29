2b03b43892f692ed55a6e7f6b0784440dbafe099
GustavoSDaniel <gustavosdaniel@hotmail.com>
2025-07-29 20:16:07 -0300

    feat: Iniciando projeto

---
+++ src/test/java/com/gustavosdaniel/restaurante/RestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavosdaniel.restaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
