65c0a02b27826e3de4927d456af377974b50431f
GustavoSDaniel <gustavosdaniel@hotmail.com>
2025-07-30 21:59:42 -0300

    feat: criando as eentidades do projetos

---
+++ src/main/java/com/gustavosdaniel/restaurante/restaurant/Restaurant.java
@@ -0,0 +1,17 @@
+package com.gustavosdaniel.restaurante.restaurant;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+@Document(indexName = restaurants")
+@Data
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+public class Restaurant {
+
+
+}
